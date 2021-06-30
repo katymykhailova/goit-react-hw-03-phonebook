a9a73dec329261c0ec5bc901d205f50a18ec2ece
kotikm81 <77439323+kotikm81@users.noreply.github.com>
2021-06-30 16:53:42 +0300

    Подчищает код

--- src/ContactList/ContactList.styles.jsx
+++ src/ContactList/ContactList.styles.jsx
@@ -19,29 +19,3 @@ export const ContactsItem = styled.li`
     margin-right: 10px;
   }
 `;
-
-// export const ContactsText = styled.span``;
-
-// export const Button = styled.button`
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   margin-left: 10px;
-//   padding: 10px 28px;
-//   max-width: 200px;
-
-//   font-weight: 700;
-//   font-size: 12px;
-//   text-align: center;
-//   letter-spacing: 0.06em;
-//   text-decoration: none;
-//   background-color: #2196f3;
-//   color: #fff;
-//   border: 0;
-//   border-radius: 4px;
-//   cursor: pointer;
-
-//   &:hover {
-//     background-color: #1e81d3;
-//   }
-// `;
